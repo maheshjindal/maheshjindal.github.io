edcb9fd851bba8d38c1e8608e951d52a13f7bd77
Mahesh Jindal <mahesh.jindal6@gmail.com>
2021-12-29 04:33:01 -0500

    Fix: Using 'HashRouter' for the page navigation

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/GlobalStyle';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle/>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
